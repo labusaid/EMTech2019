6ad3583485793ded339a237e68cd5e20ed3e7da7
Milan Harris <milanharrisdev@gmail.com>
2019-10-20 02:53:07 -0500

    Gets heatmap data from api
    (like a boss)

--- hatemap/src/components/HeatMap.js
+++ hatemap/src/components/HeatMap.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import Axios from "axios";
 
 //array of heatmap points
 var heatPoints = [];
 //Function to add a point to heatmap
 function addTweetPoint(lat, lng, weight) {
     heatPoints.push({lat: lat, lng: lng, weight: weight})
+}
+
+function addTweetData(comp, location, keyword, tone){
+    Axios.get('http://localhost:3000/getaveragetone?location=' + location + '&keyword=' + keyword)
+        .then(response => {
+            console.log(response);
+            var toneCount = tone ? response.data.positive_count : response.data.negative_count;
+            var toneAverage = tone ? response.data.positive_average : response.data.negative_average;
+            console.log('tone average: ' + toneAverage);
+            console.log('tone count: ' + toneCount);
+            var weight = toneAverage * toneCount;
+            var long = 0;
+            var lat = 0;
+            console.log(weight);
+            console.log(response.data.location);
+            Axios.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + response.data.location + '&key=AIzaSyBqEJc6UcXXbk7xC4bE_zzn59OAamXa2Xc')
+                .then(response => {
+                    lat = response.data.results[0].geometry.location.lat;
+                    long = response.data.results[0].geometry.location.lng;
+                    console.log('lat: ' + lat + ', long: ' + long);
+                    addTweetPoint(lat, long, weight);
+                    comp.render();
+                });
+        });
 }
 
 //Heatmap component
@@ -15,7 +40,7 @@ class HeatMap extends Component {
             lat: 32.6,
             lng: -99.2
         },
-        zoom: 7
+        zoom: 4
     };
 
     constructor(props) {
@@ -24,6 +49,9 @@ class HeatMap extends Component {
             heatmapVisible: true,
             heatmapPoints: heatPoints,
         }
+
+        addTweetData(this, 'losangeles', 'trump', false);
+        addTweetData(this, 'oklahomacity', 'trump', true);
     }
 
     //Code run on render
@@ -36,11 +64,6 @@ class HeatMap extends Component {
                 opacity: 0.5
             }
         };
-
-        addTweetPoint(33.0,-99.2,6);
-        addTweetPoint(32.5,-99.2,1);
-        addTweetPoint(32.0,-99.2,3);
-        addTweetPoint(31.5,-99.2,20);
 
         //Log State
         console.log(this.state);
